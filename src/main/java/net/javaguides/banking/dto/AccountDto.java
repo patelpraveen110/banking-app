30704f05c7b839abd31f0ff8c0fdfac9d65ef470
patelpraveen110 <patelpraveen110@gmail.com>
2024-02-24 22:41:13 +0530

    create account

---
+++ src/main/java/net/javaguides/banking/dto/AccountDto.java
@@ -0,0 +1,38 @@
+package net.javaguides.banking.dto;
+
+import lombok.Data;
+
+@Data
+public class AccountDto {
+	
+	public AccountDto(Long id, String accountHolderName, double balance) {
+		super();
+		this.id = id;
+		this.accountHolderName = accountHolderName;
+		this.balance = balance;
+	}
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getAccountHolderName() {
+		return accountHolderName;
+	}
+	public void setAccountHolderName(String accountHolderName) {
+		this.accountHolderName = accountHolderName;
+	}
+	public double getBalance() {
+		return balance;
+	}
+	public void setBalance(double balance) {
+		this.balance = balance;
+	}
+	private Long id;
+	private String accountHolderName;
+	private double balance;
+	
+
+
+}
---
+++ src/main/java/net/javaguides/banking/entity/Account.java
@@ -0,0 +1,54 @@
+package net.javaguides.banking.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name="accounts")
+@Entity
+public class Account {
+	
+	public Account(Long id, String accountHolderName, double balance) {
+		super();
+		this.id = id;
+		this.accountHolderName = accountHolderName;
+		this.balance = balance;
+	}
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	@Column(name="account_holder_name")
+	private String accountHolderName;
+	private double balance;
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getAccountHolderName() {
+		return accountHolderName;
+	}
+	public void setAccountHolderName(String accountHolderName) {
+		this.accountHolderName = accountHolderName;
+	}
+	public double getBalance() {
+		return balance;
+	}
+	public void setBalance(double balance) {
+		this.balance = balance;
+	}
+
+}
